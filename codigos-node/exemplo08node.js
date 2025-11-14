a0607b1d8d14354f9acf5fd3bcabe020d3af031c
renan544 <renan.rrs.santos@gmail.com>
2025-11-14 20:46:31 -0300

    Add files via upload

---
+++ codigos-node/exemplo08node.js
@@ -0,0 +1,20 @@
+// Importandoum Modulo 'fs' (file System)
+const fs = require('fs');
+
+console.log("Iniciando nosso script ...");
+
+const dia = new Date()
+const usuario = "Renan Santos"
+const mensagem = "Log do dia :" + dia.getDate() + "/" + (dia.getMonth() + 1) + "\nO usuário acessou o sistema :\n"
++ usuario + " " + dia.getDate() + "/" + (dia.getMonth()+1) + " " + dia.getHours() + " : "+ dia.getMinutes();
+
+// Escrever o arquivo de forma sincrona
+fs.writeFileSync("arquivo07.txt",mensagem)
+
+try {
+    fs.writeFileSync("arquivo07.txt",mensagem)
+    console.log("Arquivo criado com sucesso.")
+} catch (erro){
+    console.error("Desculpe-me, ocorreu um erro ao escrever o arquivo> ",erro)
+}
+console.log("Finaliznado nosso script ...");
